f1413398a23d1191ab6c4f2a331354cae3017cf8
Thiago Soares <thiagocarv4@gmail.com>
2025-04-28 23:09:57 -0300

    create users feito

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import { errorHandling } from "./middlewares/error-handling";
+import { routes } from "./routes";
+
 const app = express();
 
 const PORT = 3333;
 
 app.use(express.json());
+
+app.use(routes);
 
 app.use(errorHandling);
 
---
+++ src/controllers/users-controller.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from "express";
+import { z } from "zod";
+import { hash } from "bcrypt";
+import { prisma } from "@/config/prisma";
+import { AppError } from "@/utils/app-error";
+
+class UsersController {
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().trim().min(3),
+        email: z.string().email(),
+        password: z.string().min(6),
+      });
+
+      const { name, email, password } = bodySchema.parse(request.body);
+
+      const hashedPassword = await hash(password, 8);
+
+      const userWithSameEmail = await prisma.users.findFirst({
+        where: { email },
+      });
+
+      if (userWithSameEmail) {
+        throw new AppError("user with same email already exists");
+      }
+
+      await prisma.users.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      return response.status(201).json({ name, email });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  
+}
+
+export { UsersController };
